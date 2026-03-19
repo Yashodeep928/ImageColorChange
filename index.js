a85c69872a675921ae6eb79a81053866193ad47d
Yashodeep Deshpande <yashodeepdeshpande84639@gmail.com>
2026-03-19 21:36:31 +0530

    Working on the logic of filling color

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const canvas = document.getElementById("myCanvas")
-const ctx = canvas.getContext("2d")
-const colorPicker = document.getElementById("colorPicker")
-const btn = document.getElementById("colorBtn")
-
+const canvas = document.getElementById("myCanvas") 
+const ctx = canvas.getContext("2d") 
+const colorPicker = document.getElementById("colorPicker") 
+const btn = document.getElementById("colorBtn") 
 const img = new Image();
-img.src = "download.webp"
-img.onload = function () {
-      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-    };
-
-
-
-
-
+ img.src = "download.webp" 
+ img.onload = function () { ctx.drawImage(img, 0, 0, canvas.width, canvas.height); }; console.log(colorPicker) 
+ 
+ btn.addEventListener('click',()=>{
+ctx.fillStyle = colorPicker.value
+ ctx.globalAlpha = 0.35
+ctx.fillRect(0,0,canvas.width,canvas.height)
+ })
+ 
